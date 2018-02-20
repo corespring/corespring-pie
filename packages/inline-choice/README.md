5edb6c006120a522c8ed78d5f81f51f8b66605a7
edeustace <ed.eustace@gmail.com>
2018-02-20 14:36:00 -0500

    updates

---
+++ packages/inline-choice/README.md
@@ -0,0 +1,5 @@
+# @corespring-pie/inline-choice
+
+A port of the corespring inline-choice component.
+
+## BETA!!
---
+++ packages/inline-choice/controller/src/index.js
@@ -0,0 +1,40 @@
+import * as root from '@pie-elements/inline-choice-controller';
+
+const expandFeedback = fb => {
+  if (!fb) {
+    return {
+      type: 'default'
+    }
+  } else {
+    return {
+      type: fb.type || 'default',
+      text: fb.type === 'custom' && fb.text ? [{ lang: 'en-US', value: fb.text }] : []
+    }
+  }
+}
+
+const expandChoice = c => {
+  return {
+    label: [{ lang: 'en-US', value: c.label }],
+    feedback: expandFeedback(c.feedback)
+  }
+}
+
+/**
+ * Expand the simple corespring model into a @pie-elements/inline-choice-controller model. 
+ * @param {*} model 
+ */
+const expandModel = (model) => {
+  return {
+    defaultLang: 'en-US',
+    prompt: [
+      { lang: 'en-US', value: model.propmpt }
+    ],
+    choices: model.choices.map(expandChoice)
+  }
+}
+
+export function model(question, session, env) {
+  const q = expandModel(question);
+  return root.model(q, session, env);
+}
