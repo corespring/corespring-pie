3eda2d457fe5e7d96da52701227d7fcd6244bd9b
edeustace <ed.eustace@gmail.com>
2018-02-21 12:05:09 -0500

    fix(controller): update tests

--- packages/text-entry/controller/src/__test__/index.test.js
+++ packages/text-entry/controller/src/__test__/index.test.js
@@ -1,30 +1,127 @@
-import { model as modelMock } from '@pie-elements/text-entry-controller';
-
 import { model } from '../index';
 
 
 describe('model', () => {
 
-  test('it calls model with expanded data', async () => {
-    const question = {
-      correctResponses: {
-        values: ['apple', 'banana']
-      }
-    }
 
-    const session = {
-      value: ['apple']
-    }
-    const env = {}
-    await model(question, session, env);
-    expect(modelMock.mock.calls.length).toBe(1);
-    expect(modelMock.mock.calls[0][0]).toMatchObject({
-      correctResponses: {
-        values: [
-          { lang: 'en-US', value: 'apple' },
-          { lang: 'en-US', value: 'banana' }
-        ]
+  let result, question, session, env;
+
+  const mkQuestion = () => ({
+    correctResponses: {
+      values: ['a'],
+      feedback: {
+        type: 'default'
       }
+    },
+    partialResponses: {
+      values: ['aa'],
+      feedback: {
+        type: 'custom',
+        value: 'foo'
+      }
+    },
+    incorrectFeedback: {
+      type: 'custom',
+      value: 'foo'
+    }
+  });
+
+  describe('gather', () => {
+
+    beforeEach(async () => {
+      question = mkQuestion();
+      session = { value: 'a' }
+      env = { mode: 'gather' }
+      result = await model(question, session, env);
     });
+
+    it('returns color_contrast', () => {
+      expect(result.colorContrast).toEqual('black_on_white');
+    });
+    it('returns undefined for numbers only warning', () => {
+      expect(result.numbersOnlyWarning).toEqual(undefined);
+    });
+
+    it('returns disabled:false', () => {
+      expect(result.disabled).toEqual(false);
+    });
+
+    it('returns undefined for correctness ', () => {
+      expect(result.correctness).toEqual(undefined);
+    });
+
+    it('returns undefined for feedback', () => {
+      expect(result.feedback).toEqual(undefined);
+    });
+
+  });
+
+  describe('view', () => {
+
+    beforeEach(async () => {
+      question = mkQuestion();
+      session = { value: 'a' }
+      env = { mode: 'view' }
+      result = await model(question, session, env);
+    });
+
+    it('returns disabled:true', () => {
+      expect(result.disabled).toEqual(true);
+    });
+
+    it('returns undefined for correctness ', () => {
+      expect(result.correctness).toEqual(undefined);
+    });
+
+    it('returns default correct for feedback', () => {
+      expect(result.feedback).toEqual(undefined);
+    });
+
+  });
+
+  describe('evaluate - correct', () => {
+
+    beforeEach(async () => {
+      question = mkQuestion();
+      session = { value: 'a' }
+      env = { mode: 'evaluate' }
+      result = await model(question, session, env);
+    });
+
+    it('returns disabled:true', () => {
+      expect(result.disabled).toEqual(true);
+    });
+
+    it('returns correct for correctness ', () => {
+      expect(result.correctness).toEqual('correct');
+    });
+
+    it('returns default correct for feedback', () => {
+      expect(result.feedback).toEqual('Correct');
+    });
+
+  });
+
+  describe('evaluate - partially correct', () => {
+
+    beforeEach(async () => {
+      question = mkQuestion();
+      session = { value: 'aa' }
+      env = { mode: 'evaluate' }
+      result = await model(question, session, env);
+    });
+
+    it('returns disabled:true', () => {
+      expect(result.disabled).toEqual(true);
+    });
+
+    it('returns partially-correct for correctness ', () => {
+      expect(result.correctness).toEqual('partially-correct');
+    });
+
+    it('returns custom correct for feedback', () => {
+      expect(result.feedback).toEqual('foo');
+    });
+
   });
 });
--- packages/text-entry/controller/src/index.js
+++ packages/text-entry/controller/src/index.js
@@ -2,80 +2,6 @@
 import debug from 'debug';
 
 const log = debug('@corespring-pie:text-entry:controller');
-// const expandValues = (values, fb) => {
-
-//   return values.map(v => {
-
-//     const feedback = (() => {
-//       if (!fb) {
-//         return undefined;
-//       } else if (fb.type === 'none') {
-//         return undefined;
-//       } else if (fb.type === 'default') {
-//         return 'DEFAULT';
-//       } else if (fb.type === 'custom') {
-//         return fb.value;
-//       }
-//     })();
-
-//     return {
-//       lang: 'en-US',
-//       value: v,
-//       feedback
-//     }
-//   })
-// }
-
-// const expandResponses = (r = { values: [], feedback: {} }) => ({
-//   values: expandValues(r.values, r.feedback),
-//   ignoreWhitespace: r.ignoreWhitespace,
-//   ignoreCase: r.ignoreCase
-// });
-
-// /**
-//  * @param {*} ifb 
-//  */
-// const expandIncorrectFeedback = ifb => {
-//   if (!ifb) {
-//     return {
-//       disabled: true
-//     }
-//   }
-
-//   if (ifb.type === 'none') {
-//     return { disabled: true }
-//   }
-
-//   if (ifb.type === 'custom') {
-//     return {
-//       fallback: {
-//         values: [
-//           { lang: 'en-US', feedback: ifb.value }
-//         ]
-//       }
-//     }
-//   }
-
-//   if (ifb.type === 'default') {
-//     return {
-//       fallback: {
-//         values: [
-//           { lang: 'en-US', feedback: 'Incorrect' }
-//         ]
-//       }
-//     }
-//   }
-// };
-
-
-// const expandModel = (q) => ({
-//   defaultLang: 'en-US',
-//   correctResponses: expandResponses(q.correctResponses),
-//   partialResponses: expandResponses(q.partialResponses),
-//   incorrectFeedback: expandIncorrectFeedback(q.incorrectFeedback),
-//   model: q.model
-// });
-
 
 const process = (v, ignoreCase, ignoreWhitespace) => {
   let out = v ? v.trim() : '';
@@ -91,9 +17,6 @@ const inResponses = (responses, value) => {
 }
 
 export function model(question, session, env) {
-  // const q = expandModel(question);
-  // return root.model(q, session, env);
-  log('[model] ...');
   return new Promise((resolve, reject) => {
 
     const { model, correctResponses } = question;
